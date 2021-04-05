db9af3031095caa4ca509bc500b04d1d7106131e
Basem98 <basem.mostafa98@hotmail.com>
2021-04-05 15:42:57 +0200

    feat: Apply sweep to left animation to nav items

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+/**
+ * Global Variables
+ */
+
+/**
+ * A list of the navigation items in the side navigation bar
+ */
+const navItems = document.getElementsByClassName('nav-items').item(0);
+
+
+/**
+ * Helper Functions
+ */
+
+
+/**
+ * @description Change the styling of a navigation item
+ * when it's clicked
+ * @param {Event} event An event object that's based on the main Event interface,
+ * and has properties specific to the dispatched event
+ */
+
+function activateNavItem(event) {
+    const clickedAnchor = event.target;
+    for (let i = 0; i < navItems.children.length; i += 1) {
+        const currentListItem = navItems.children.item(i);
+        currentListItem.classList.remove('active-nav-item');
+        currentListItem.children.item(0).classList.remove('active-nav-anchor');
+    }
+    clickedAnchor.classList.add('active-nav-anchor');
+    clickedAnchor.parentElement.classList.add('active-nav-item');
+}
+
+
+/**
+ * Events
+ */
+
+
+/**
+ * When a navigation item is clicked, change its color and its background
+ */
+for (let i = 0; i < navItems.children.length; i += 1) {
+    const currentAnchor = navItems.children.item(i).children.item(0);
+    currentAnchor.addEventListener('click', activateNavItem);
+}
